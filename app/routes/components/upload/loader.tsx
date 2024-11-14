a34a640314743f7fea41dfb6988b66d2b063234f
Adi Radh <adiradh1998@gmail.com>
2024-11-14 08:19:21 -0700

    add loading text

---
+++ app/routes/components/upload/loader.tsx
@@ -0,0 +1,7 @@
+import style from './loader.module.css';
+
+export function Loader() {
+  return (
+    <div className={`${style.loader}`}></div>
+  );
+}
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -8,6 +8,8 @@ import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import { getUserId } from "~/session.server";
 import { createVideo } from "~/models/video.server";
 import * as React from "react";
+import { Loader } from "./components/upload/loader";
+import { createPortal } from 'react-dom';
 
 export const meta: MetaFunction = () => {
     return [{
@@ -30,6 +32,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
   
 export const action: ActionFunction = async ({ request }) => {
+
     const formData = await request.formData();
     const userId = await getUserId(request);
     const video = formData.get("video");
@@ -66,10 +69,10 @@ export const action: ActionFunction = async ({ request }) => {
         );
     }
 };
-
 export default function Upload() {
     const actionData = useActionData() as ActionData;
     const videoRef = React.useRef<HTMLInputElement>(null);
+    const [isSubmitting, setIsSubmitting] = React.useState(false);
 
     React.useEffect(() => {
         if (actionData?.errors?.video) {
@@ -77,74 +80,80 @@ export default function Upload() {
         }
     }, [actionData]);
 
+    const handleSubmit = () => {
+        setIsSubmitting(true);
+    };
+
     return (
         <div className="flex min-h-full flex-col justify-center">
             <div className="mx-auto w-full max-w-md px-8">
-            <Form className="space-y-6" method="post" encType="multipart/form-data">
-                <div>
-                <label className="text-sm font-medium" htmlFor="title">
-                    <span className="block text-mustard">Title</span>
-                    {actionData?.errors?.title && (
-                    <span className="block pt-1 text-red" id="title-error">
-                        {actionData?.errors?.title}
-                    </span>
-                    )}
-                </label>
-                <input
-                    className="w-full rounded border border-mustard px-2 py-1 text-lg"
-                    type="text"
-                    name="title"
-                    id="title"
-                    required
-                    aria-invalid={actionData?.errors?.title ? true : undefined}
-                    aria-describedby="title-error"
-                />
-                </div>
-                <div>
-                <label className="text-sm font-medium" htmlFor="description">
-                    <span className="block text-mustard">Description</span>
-                    {actionData?.errors?.description && (
-                    <span className="block pt-1 text-red" id="description-error">
-                        {actionData?.errors?.description}
-                    </span>
-                    )}
-                </label>
-                <textarea
-                    className="w-full rounded border border-mustard px-2 py-1 text-lg"
-                    name="description"
-                    id="description"
-                    required
-                    aria-invalid={actionData?.errors?.description ? true : undefined}
-                    aria-describedby="description-error"
-                />
-                </div>
-                <div>
-                <label className="text-sm font-medium" htmlFor="video">
-                    <span className="block text-mustard">Video File (Not larger than 200MB)</span>
-                    {actionData?.errors?.video && (
-                    <span className="block pt-1 text-red" id="video-error">
-                        {actionData?.errors?.video}
-                    </span>
-                    )}
-                </label>
-                <input
-                    className="w-full rounded border border-mustard px-2 py-1 text-lg"
-                    type="file"
-                    name="video"
-                    id="video"
-                    required
-                    aria-invalid={actionData?.errors?.video ? true : undefined}
-                    aria-describedby="video-error"
-                    ref={videoRef}
-                />
-                </div>
-                <button
-                className="w-full rounded bg-amber py-2 px-4 text-white hover:bg-mustard focus:bg-amber"
-                type="submit"
-                >
-                Upload Video
-                </button>
-            </Form>
+                <Form className="space-y-6" method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
+                    <div>
+                        <label className="text-sm font-medium" htmlFor="title">
+                            <span className="block text-mustard">Title</span>
+                            {actionData?.errors?.title && (
+                                <span className="block pt-1 text-red" id="title-error">
+                                    {actionData?.errors?.title}
+                                </span>
+                            )}
+                        </label>
+                        <input
+                            className="w-full rounded border border-mustard px-2 py-1 text-lg"
+                            type="text"
+                            name="title"
+                            id="title"
+                            required
+                            aria-invalid={actionData?.errors?.title ? true : undefined}
+                            aria-describedby="title-error"
+                        />
+                    </div>
+                    <div>
+                        <label className="text-sm font-medium" htmlFor="description">
+                            <span className="block text-mustard">Description</span>
+                            {actionData?.errors?.description && (
+                                <span className="block pt-1 text-red" id="description-error">
+                                    {actionData?.errors?.description}
+                                </span>
+                            )}
+                        </label>
+                        <textarea
+                            className="w-full rounded border border-mustard px-2 py-1 text-lg"
+                            name="description"
+                            id="description"
+                            required
+                            aria-invalid={actionData?.errors?.description ? true : undefined}
+                            aria-describedby="description-error"
+                        />
+                    </div>
+                    <div>
+                        <label className="text-sm font-medium" htmlFor="video">
+                            <span className="block text-mustard">Video File (Not larger than 200MB)</span>
+                            {actionData?.errors?.video && (
+                                <span className="block pt-1 text-red" id="video-error">
+                                    {actionData?.errors?.video}
+                                </span>
+                            )}
+                        </label>
+                        <input
+                            className="w-full rounded border border-mustard px-2 py-1 text-lg"
+                            type="file"
+                            name="video"
+                            id="video"
+                            required
+                            aria-invalid={actionData?.errors?.video ? true : undefined}
+                            aria-describedby="video-error"
+                            ref={videoRef}
+                        />
+                    </div>
+                    <button
+                        className="w-full rounded bg-amber py-2 px-4 text-white hover:bg-mustard focus:bg-amber"
+                        type="submit"
+                        disabled={isSubmitting}
+                    >
+                        {isSubmitting ? "Uploading..." : "Upload Video"}
+                    </button>
+                    </Form>
+                    {isSubmitting  ? createPortal(<Loader show={true}/>, document.body): null}
             </div>
         </div>
     );
